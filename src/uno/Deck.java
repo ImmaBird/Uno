60e59b4227579a0a8d6a8ce74be99b7b670850b9
immabird <immapokeyew@gmail.com>
2019-03-27 20:40:34 -0400

    deck

--- src/uno/Deck.java
+++ src/uno/Deck.java
@@ -1,38 +1,26 @@
 package uno;
 
-import java.util.ArrayList;
-
-/**
- * 
- * @author borgonovog deck is just a pile, but with an order
- */
 public class Deck extends Pile {
 
-	/**
-	 * default constructor
-	 */
 	public Deck() {
-		String[] color = { "red", "blue", "green", "yellow", "none"};
-		for (int i = 0; i < color.length; i++) {
-			this.addCard(new Card(color[i], 0));
-			this.addCard(new Card("wild", 13));
-			this.addCard(new Card("wild", 14));
-			for (int j = 1; j < 13; j++) {
-				this.addCard(new Card(color[i], j));
-				this.addCard(new Card(color[i], j));
+		Card.Color[] colors = Card.Color.values();
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j <= 12; j++) {
+				if (j <= 9) {
+					this.addCard(new Card(colors[i], j));
+				} else {
+					this.addCard(new Card(colors[i], j));
+					this.addCard(new Card(colors[i], j));
+				}
 			}
+			this.addCard(new Card(colors[4], 13));
+			this.addCard(new Card(colors[4], 14));
 		}
 		this.shuffle();
 	}
-	
-	public Deck(ArrayList<Card> deck) {
-		for(Card aCard : deck) {
-			this.addCard(aCard);
-		}
-	}
 
 	public void deal(Hand hand) {
-		for (int j = 0; j <= 7; j++) {
+		for (int j = 0; j < 7; j++) {
 			hand.addCard(this.removeCard());
 		}
 	}
